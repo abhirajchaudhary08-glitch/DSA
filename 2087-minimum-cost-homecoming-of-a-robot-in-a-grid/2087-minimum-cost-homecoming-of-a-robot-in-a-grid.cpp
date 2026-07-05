a0272a186619aa64b05f19ccf5209803df5fc0a5
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-07-05 21:15:57 +0530

    Time: 2 ms (24.25%), Space: 153.7 MB (54.82%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -1,15 +1,48 @@
+// class Solution {
+// public:
+//     int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
+//         int totcost = 0;
+//         int rdir = (startPos[0]<homePos[0]) ? 1:-1;
+//         for(int i =startPos[0]+rdir;i!=homePos[0]+rdir;i+=rdir){
+//             totcost+=rowCosts[i];
+//         }
+//         int cDir = (startPos[1] < homePos[1]) ? 1 : -1;
+//         for (int j = startPos[1] + cDir; j != homePos[1] + cDir; j += cDir) {
+//             totcost += colCosts[j];
+//         }
+//         return totcost;
+//     }
+// };
 class Solution {
 public:
-    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
-        int totcost = 0;
-        int rdir = (startPos[0]<homePos[0]) ? 1:-1;
-        for(int i =startPos[0]+rdir;i!=homePos[0]+rdir;i+=rdir){
-            totcost+=rowCosts[i];
+    int minCost(vector<int>& startPos, vector<int>& homePos,
+                vector<int>& rowCosts, vector<int>& colCosts) {
+
+        int sx = startPos[0], sy = startPos[1];
+        int tx = homePos[0], ty = homePos[1];
+
+        int cost = 0;
+
+       
+        while (sx < tx) {
+            sx++;
+            cost += rowCosts[sx];
         }
-        int cDir = (startPos[1] < homePos[1]) ? 1 : -1;
-        for (int j = startPos[1] + cDir; j != homePos[1] + cDir; j += cDir) {
-            totcost += colCosts[j];
+        while (sx > tx) {
+            sx--;
+            cost += rowCosts[sx];
         }
-        return totcost;
+
+        
+        while (sy < ty) {
+            sy++;
+            cost += colCosts[sy];
+        }
+        while (sy > ty) {
+            sy--;
+            cost += colCosts[sy];
+        }
+
+        return cost;
     }
 };
