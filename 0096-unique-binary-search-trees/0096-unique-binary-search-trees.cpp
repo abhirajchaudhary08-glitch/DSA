22562ee67d462578c9ab32001ff6510f324d1e66
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-07-07 17:30:14 +0530

    Time: 1129 ms (11.35%), Space: 7.7 MB (98.86%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int f(int n){
+        if(n==0 or n==1) return 1;
+        if(n==2) return 2;
+        int sum = 0;
+        for(int k = 1;k<= n;k++){
+            //k->potential roots
+            sum+= f(k-1)*f(n-k);
+        }
+        return sum;
+    }
+    int numTrees(int n) {
+        return f(n);
+    }
+};
