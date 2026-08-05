4d540811ec3ebca562235a38c3c69d70126927a1
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-08-05 18:03:14 +0530

    Time: 81 ms (34.22%), Space: 53.9 MB (93.82%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,19 +2,10 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.size();
-        int counts[26] = {0};
-        for(int i=0;i<n/2;i++){
-            counts[s[i]-'a']++;
-        }
-        int idx = 0;
-        for(int i=0;i<26;i++){
-            while(counts[i]-- > 0){
-                s[idx++] = (char)(i+'a');
-            }
-        }
-        for(int i=0;i<n/2;i++){
-            s[n-1-i] = s[i];
-        }
+       sort(s.begin(),s.begin()+n/2);
+       for(int i=0;i<n/2;i++){
+        s[n-1-i] = s[i];
+       }
         return s;
     }
 };
