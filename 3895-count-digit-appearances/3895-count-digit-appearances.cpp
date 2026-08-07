aa7a920a48cbb56ff8ab101682d555ba4728a2df
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-08-08 02:31:12 +0530

    Time: 0 ms (100%), Space: 37.1 MB (48.18%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.cpp
+++ 3895-count-digit-appearances/3895-count-digit-appearances.cpp
@@ -1,24 +1,30 @@
-class Solution {
+//       string s = "";
+// int ct = 0;
+// for(int x : nums) {
+//     s += to_string(x);
+// }
+// // 2. Convert integer d to character (if d is an int)
+// char targetChar = d + '0'; 
+// for(int i = 0; i < s.length(); i++){
+//     if(s[i] == targetChar){
+//         ct++;
+//     }
+// }
+
+// return ct;
+    class Solution {
 public:
-    int countDigitOccurrences(vector<int>& nums, int d) {
-      string s = "";
-int ct = 0;
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        int count=0;
 
-// 1. Build the string
-for(int x : nums) {
-    s += to_string(x);
-}
-
-// 2. Convert integer d to character (if d is an int)
-char targetChar = d + '0'; 
-
-// 3. Count occurrences
-for(int i = 0; i < s.length(); i++){
-    if(s[i] == targetChar){
-        ct++;
-    }
-}
-
-return ct;
+        for(int n:nums){
+            while(n){
+                int r=n%10;
+                n/=10;
+                if(r==digit)    count++;
+            }
+        }
+        return count;
     }
 };
+    
