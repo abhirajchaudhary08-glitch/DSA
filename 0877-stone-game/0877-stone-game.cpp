3b720a2489e62e2d172c2e874b59a2141bcc10be
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-08-11 19:57:24 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.83%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    // int t[502][502];
-    // bool visited[502][502];
-    // int f(vector<int>& nums,int left,int right){
-    //     if(left>right) return 0;
-    //     if(visited[left][right]){
-    //         return t[left][right];
-    //     }
-    //     visited[left][right] = true;
-    //     int takeleft = nums[left]-f(nums,left+1,right);
-    //     int takeright = nums[right]-f(nums,left,right-1);
-    //     t[left][right] = max(takeleft,takeright);
-    //     return t[left][right];
-    // }
-    // bool stoneGame(vector<int>& nums) {
-    //     int n = nums.size();
-    //     memset(visited,false,sizeof(visited));
-    //     return f(nums,0,n-1)>0;
-    // }
-        bool stoneGame(vector<int>& piles) {
+    bool stoneGame(vector<int>& piles) {
         return true;
     }
-
 };
