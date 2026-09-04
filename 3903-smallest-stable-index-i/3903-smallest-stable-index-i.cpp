60c4d9dc675f31e572b45fa535c8fd2f3b9917ca
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-09-05 03:28:49 +0530

    Time: 0 ms (100%), Space: 30.3 MB (85.64%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,19 +1,39 @@
+// class Solution {
+// public:
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; ++i) {
+//             int maxValue = nums[i], minValue = nums[i];
+//             for (int j = 0; j < i; ++j) {
+//                 maxValue = max(maxValue, nums[j]);
+//             }
+//             for (int j = i + 1; j < n; ++j) {
+//                 minValue = min(minValue, nums[j]);
+//             }
+//             if (maxValue - minValue <= k) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
+    inline static int suf[100];
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            int maxValue = nums[i], minValue = nums[i];
-            for (int j = 0; j < i; ++j) {
-                maxValue = max(maxValue, nums[j]);
-            }
-            for (int j = i + 1; j < n; ++j) {
-                minValue = min(minValue, nums[j]);
-            }
-            if (maxValue - minValue <= k) {
+        suf[n - 1] = nums.back();
+
+        for (int i = n - 2; i >= 0; i--)
+            suf[i] = min(suf[i + 1], nums[i]);        
+
+        int maxSoFar = 0;
+        for (int i = 0; i < n; i++) {
+            maxSoFar = max(maxSoFar, nums[i]);
+            if (maxSoFar - suf[i] <= k)
                 return i;
-            }
         }
+
         return -1;
     }
 };
