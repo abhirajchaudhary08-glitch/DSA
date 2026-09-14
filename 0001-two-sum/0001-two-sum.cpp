6a9388bea3bf24068a367d7fd14199400a059252
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-09-15 01:22:18 +0530

    Time: 4 ms (53.04%), Space: 14.8 MB (58.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,30 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int i;
+//         int j;
+//         for(i = 0; i<nums.size();i++){
+//             for(j= i+1; j<nums.size(); j++)
+//             if(nums[j]==target - nums[i]){
+//                 return {i,j};
+//             }
+//         }
+//         return {i,j};
+        
+//     }
+    
+// };
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i;
-        int j;
-        for(i = 0; i<nums.size();i++){
-            for(j= i+1; j<nums.size(); j++)
-            if(nums[j]==target - nums[i]){
-                return {i,j};
-            }
+        unordered_map<int,int>mp;
+
+        for(int i=0; i<nums.size(); i++){
+            int x=target-nums[i];
+            if(mp.count(x))
+                return {mp[x], i};
+            mp[nums[i]] = i;
         }
-        return {i,j};
-        
+        return {};
     }
-    
 };
