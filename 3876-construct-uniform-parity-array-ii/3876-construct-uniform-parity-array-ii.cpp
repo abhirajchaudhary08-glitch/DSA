33ad0a8d1b74e5fb4560264d1e3a4ceea2c75348
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-09-16 02:55:18 +0530

    Time: 0 ms (100%), Space: 165.7 MB (83.53%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,26 +1,45 @@
+// class Solution {
+// public:
+//     bool uniformArray(vector<int>& arr) {
+//         int n=arr.size();
+//         int e=0;
+//         int o=0;
+//         int smallest_odd=INT_MAX;
+//         for(int i=0;i<n;i++){
+//             if(arr[i]%2==0) e++;
+//             else {
+//                 o++;
+//                 smallest_odd=min(smallest_odd,arr[i]);
+//             }
+//         }
+//         if(e==n) return true;
+//         else if(o==n) return true;
+//         else {
+//            for(int i=0;i<n;i++){
+//                if(arr[i] % 2==0){
+//                    if(arr[i]<smallest_odd) return false;
+//                }
+//            } 
+//         }
+//     return true;
+//     }
+// };
 class Solution {
 public:
-    bool uniformArray(vector<int>& arr) {
-        int n=arr.size();
-        int e=0;
-        int o=0;
-        int smallest_odd=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(arr[i]%2==0) e++;
-            else {
-                o++;
-                smallest_odd=min(smallest_odd,arr[i]);
+    bool uniformArray(vector<int>& nums1) {
+        int mini=INT_MAX;
+        bool odd=false;
+        for(auto t:nums1){
+            
+            if(t%2){
+                odd=true;
+                mini=min(mini,t);
             }
         }
-        if(e==n) return true;
-        else if(o==n) return true;
-        else {
-           for(int i=0;i<n;i++){
-               if(arr[i] % 2==0){
-                   if(arr[i]<smallest_odd) return false;
-               }
-           } 
+        if(!odd) return true;
+        for(int t:nums1){
+            if(t%2==0 && mini>t) return false;
         }
-    return true;
+        return true;
     }
 };
