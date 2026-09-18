10237b8b70b8bdc671599b347f48933a71c359ac
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-09-19 02:51:08 +0530

    Time: 15 ms (69.38%), Space: 18.7 MB (76.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -22,29 +22,23 @@
 //         return maxct;
 //     }
 // };
- class Solution {
+class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        if (n == 0) return 0;
-        int res = 0;
-
-        for (int i = 0; i < n; i++) {
-            // This vector tracks visited characters for the substring starting at 'i'.
-            // It is reset for each new starting position.
-            vector<bool> vis(128, false); // Using 128 for full ASCII support
-            for (int j = i; j < n; j++) {
-                // If the current character has already been seen in this substring
-                if (vis[s[j]]) {
-                    break;
-                }
-                // Otherwise, update the result and mark the character as seen
-                else {
-                    res = max(res, j - i + 1);
-                    vis[s[j]] = true;
-                }
-            }
-        }
-        return res;
+  int lengthOfLongestSubstring(string s) {
+    // char seen_at[256];
+    // std::memset(seen_at, -1, 256);
+    std::array<int, 256> seen_at;
+    seen_at.fill(-1);
+    int maxLen = 0, lastPos = -1, slen = s.size();
+    for (int i=0;i<slen;i++) {
+      if (seen_at[s[i]] != -1 && lastPos < seen_at[s[i]]) {
+        lastPos = seen_at[s[i]];
+      }
+      if (( i - lastPos ) > maxLen) {
+        maxLen = i - lastPos;
+      }
+      seen_at[s[i]] = i;
     }
+    return maxLen;
+  }
 };
