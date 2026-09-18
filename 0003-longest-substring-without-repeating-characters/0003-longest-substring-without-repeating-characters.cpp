368d1bed29ff9d37b1c14178f62dab8becc44a40
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-09-19 02:50:44 +0530

    Time: 463 ms (5.05%), Space: 81 MB (26.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,50 @@
-class Solution {
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int ct = 0;
+//         int maxct = 0;
+//         string p = "";
+        
+//         for(int i = 0; i < s.length(); i++) {
+//             if(p.find(s[i]) == string::npos) {
+//                 p.push_back(s[i]);  
+//                 ct++; 
+//             } 
+//             else {
+//                 size_t foundPos = p.find(s[i]);
+//                 // Remove everything from the start up to and including that duplicate
+//                 p.erase(0, foundPos + 1);
+//                 p.push_back(s[i]);
+//                 ct = p.length();
+//             }
+//             maxct = max(maxct, ct);
+//         }
+//         return maxct;
+//     }
+// };
+ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ct = 0;
-        int maxct = 0;
-        string p = "";
-        
-        for(int i = 0; i < s.length(); i++) {
-            if(p.find(s[i]) == string::npos) {
-                p.push_back(s[i]);  
-                ct++; 
-            } 
-            else {
-                size_t foundPos = p.find(s[i]);
-                // Remove everything from the start up to and including that duplicate
-                p.erase(0, foundPos + 1);
-                p.push_back(s[i]);
-                ct = p.length();
+        int n = s.length();
+        if (n == 0) return 0;
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            // This vector tracks visited characters for the substring starting at 'i'.
+            // It is reset for each new starting position.
+            vector<bool> vis(128, false); // Using 128 for full ASCII support
+            for (int j = i; j < n; j++) {
+                // If the current character has already been seen in this substring
+                if (vis[s[j]]) {
+                    break;
+                }
+                // Otherwise, update the result and mark the character as seen
+                else {
+                    res = max(res, j - i + 1);
+                    vis[s[j]] = true;
+                }
             }
-            maxct = max(maxct, ct);
         }
-        return maxct;
+        return res;
     }
 };
