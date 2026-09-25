d52b5f7a97a2921bd9fcb5c5b5515b9456cbc7f7
abhirajchaudhary08-glitch <abhirajchaudhary08@gmail.com>
2026-09-26 02:59:30 +0530

    Time: 0 ms (100%), Space: 14.4 MB (14.08%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,25 +1,53 @@
+// class Solution {
+// public:
+//     int bagOfTokensScore(vector<int>& tokens, int power) {
+//       int n= tokens.size();
+//       int mxscore = 0;
+//       sort(tokens.begin(),tokens.end());
+//       int i=0,j=n-1;
+//       int score = 0;
+//       while(i<=j){
+//         if(power>=tokens[i]){
+//             power-=tokens[i];
+//             score+=1;
+//             i++;
+//             mxscore = max(mxscore,score);
+//         }else if(score>=1){
+//             power+=tokens[j];
+//             score-=1;
+//             j--;
+//         }else{
+//             return mxscore;
+//         }
+//       }  
+//       return mxscore;
+//     }
+// };
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-      int n= tokens.size();
-      int mxscore = 0;
-      sort(tokens.begin(),tokens.end());
-      int i=0,j=n-1;
-      int score = 0;
-      while(i<=j){
-        if(power>=tokens[i]){
-            power-=tokens[i];
-            score+=1;
-            i++;
-            mxscore = max(mxscore,score);
-        }else if(score>=1){
-            power+=tokens[j];
-            score-=1;
-            j--;
-        }else{
-            return mxscore;
+        sort(tokens.begin(), tokens.end());
+        int n = tokens.size();
+        int score = 0;
+        int max_score = 0;
+        int left = 0;
+        int right = n - 1;
+        
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                power -= tokens[left];
+                score += 1;
+                left += 1;
+                max_score = max(max_score, score);
+            } else if (score > 0) {
+                power += tokens[right];
+                score -= 1;
+                right -= 1;
+            } else {
+                break;
+            }
         }
-      }  
-      return mxscore;
+        
+        return max_score;
     }
 };
